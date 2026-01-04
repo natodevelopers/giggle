e6ec18ea525083eb27ff4042a89f117dd69261cf
natodevelopers <developers.nato@gmail.com>
2026-01-04 20:40:41 +0700

    Update GitHub link to natodevelopers/giggle

--- components/shared/Footer/index.js
+++ components/shared/Footer/index.js
@@ -11,7 +11,7 @@ export default function Footer() {
             <div className={styles.footer__text}>
                 {/* <p className={styles.location} ><BsGeoAlt /> {location} </p> */}
                 <GhStarBtn />
-                <p> <span className={styles.logo}>Giggle</span> is a Free and Open Source project. <br /> You can view its source code on <a href="https://github.com/devxprite/searchex" target="_blank">Github</a> </p>
+                <p> <span className={styles.logo}>Giggle</span> is a Free and Open Source project. <br /> You can view its source code on <a href="https://github.com/natodevelopers/giggle" target="_blank">Github</a> </p>
                 <ThemeBtn />
             </div>
         </footer>
